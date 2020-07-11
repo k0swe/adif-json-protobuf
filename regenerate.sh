58a8b1d63e5f9b5cac1d9078090fda4d5ac1239a
Chris Keller <xylo04@gmail.com>
2020-07-10 22:24:31 -0600

    protoc-gen-jsonschema merged the timestamp change, remove workaround

--- regenerate.sh
+++ regenerate.sh
@@ -6,13 +6,6 @@ fi
 if ! command -v protoc-gen-jsonschema &> /dev/null; then
   echo "Need to install protoc-gen-jsonschema"
   go get github.com/chrusty/protoc-gen-jsonschema/cmd/protoc-gen-jsonschema
-
-  # workaround for https://github.com/chrusty/protoc-gen-jsonschema/issues/11
-  pwd=`pwd`
-  cd `go env GOPATH`/src/github.com/chrusty/protoc-gen-jsonschema/cmd/protoc-gen-jsonschema/
-  git checkout -t origin/timestamps_as_strings
-  go install
-  cd $pwd
 fi
 
 protoc --jsonschema_out=./jsonschema/ --python_out=./demo/ adif.proto
